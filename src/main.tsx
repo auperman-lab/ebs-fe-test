19e0f966d7f1004c5b6bafed07dd6b6c1c4f9939
auperman-lab <121299815+auperman-lab@users.noreply.github.com>
2025-05-19 13:18:18 +0300

    change routing method

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+
+import {HashRouter, Routes, Route} from "react-router-dom";
 import './index.css'
 import HomePage from "./pages/HomePage.tsx";
 import ErrorPage from "./pages/ErrorPage.tsx";
@@ -11,28 +9,18 @@ import CartPage from "./pages/CartPage.tsx";
 import Layout from "./components/layout/Layout.tsx";
 import {CartProvider} from "./context/CartContext.tsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-      {
-        path: "cart",
-        element: <CartPage />,
-      },
-    ],
-  },
-]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <CartProvider>
-      <RouterProvider router={router} />
-    </CartProvider>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="cart" element={<CartPage />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Route>
+        </Routes>
+      </HashRouter>    </CartProvider>
   </StrictMode>,
 )
